5e43b88191d74ded1d63a1ad02485cc6cd166d44
Skullslice <53416455+Skullslice@users.noreply.github.com>
2025-09-26 15:16:05 -0700

    Update Auto Grinder.js

--- Auto Grinder.js
+++ Auto Grinder.js
@@ -1,9 +1,133 @@
-var mapHeight = GlobalVars.getDouble("spawnHeight")
-var sneak = true
-var reconnect = true
-var finder = false
+var enabled = false; //the bot should be off when cluient starts.
+var sneak = true; //occasionally sneaks in middle.
+var bow = true; //occasionally uses the bow and aims and shoots.
+var finder = true;
+var waterbreak = true;
 var quality = 0.45
 var minimum = 2
+
+var currentMap = null;
+var spawnY = 0;
+var groundY = 0;
+var prestige = {x: 0, y: 0, z: 0};
+var items = {x: 0, y: 0, z: 0};
+var upgrades = {x: 0, y: 0, z: 0};
+var mapInfo = [];
+
+JsMacros.on("JoinServer", JavaWrapper.methodToJava( event => {
+    JsMacros.waitforevent("ChunkLoad");
+    Client.waitTick(100); //wait 5 seconds for world to fully load
+    getMap();
+    
+});
+
+JsMacros.on("Key", JavaWrapper.methodToJava( event => {
+    if (event.key == "key.keyboard.i" && event.action === 1) {
+        const reverse = !GlobalVars.getBoolean("ToggleScript");
+        GlobalVars.putBoolean("ToggleScript", reverse);
+        if (reverse) {
+            iterations = {final: false, count: 1};
+            GlobalVars.putInt("iterations", 1);
+            Chat.log("\u00a74> \u00a73[\u00a7cAfk\u00a73] \u00a74> \u00a7aWaiting for afk kick ->" + " \u00a7b#" + iterations.count);
+        }
+        else {
+            iterations = {final: false, count: "\u00a7f[\u00a78Disabled\u00a7f]"};
+            Chat.log("\u00a74> \u00a73[\u00a7cAfk\u00a73] \u00a74> \u00a7f[\u00a78Disabled\u00a7f]");
+        }
+        return true;
+    }
+    return false;
+}));
+
+function getMap() {
+    var kings = World.getBlock(-11, 95, 6)?.getId().toString() == "minecraft:ender_chest";
+    var corals = World.getBlock(0, 0, 0)?.getId().toString() == "minecraft:ender_chest";
+    var ogmap = World.getBlock(-13, 114, 7)?.getId().toString() == "minecraft:ender_chest";
+    var seasons = World.getBlock(-12, 114, 5)?.getId().toString() == "minecraft:ender_chest";
+    var genesis = World.getBlock(0, 0, 0)?.getId().toString() == "minecraft:ender_chest";
+    var limbo = World.getBlock(-21, 33, 23)?.getId().toString() == "minecraft:torch" && World.getBlock(-21, 33, 19)?.getId().toString() == "minecraft:torch";
+    var hypixelHub = World?.getScoreboards()?.getCurrentScoreboard()?.getName()?.includes("MainScoreboard");
+    
+    //get the current map and set locations.
+    if (kings == true) {
+        currentMap = "kings"
+        spawnY = 50
+        groundY = 40
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", 70);
+        GlobalVars.putDouble("mapHeight", 80);
+        
+    }
+    else if (corals == true) {
+        currentMap = "corals"
+        spawnY = 50
+        groundY = 40
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", 70);
+        GlobalVars.putDouble("mapHeight", 80);
+    }
+    else if (ogmap == true) {
+        currentMap = "ogmap"
+        spawnY = 50
+        groundY = 40
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", 70);
+        GlobalVars.putDouble("mapHeight", 80);
+    }
+    else if (seasons == true) {
+        currentMap = "seasons"
+        spawnY = 50
+        groundY = 40
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", 70);
+        GlobalVars.putDouble("mapHeight", 80);
+    }
+    else if (genesis == true) {
+        currentMap = "genesis"
+        spawnY = 50
+        groundY = 40
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", 70);
+        GlobalVars.putDouble("mapHeight", 80);
+    }
+    else if (hypixelHub == true) {
+        Chat.say("/play pit");
+    }
+    else if (limbo == true) {
+        Client.disconnect();
+    }
+    else {
+        currentMap = "unknown"
+        spawnY = -1
+        groundY = -1
+        prestige = {x: 0, y: 0, z: 0}
+        items = {x: 0, y: 0, z: 0}
+        upgrades = {x: 0, y: 0, z: 0}
+        GlobalVars.putDouble("clickHeight", -1);
+        GlobalVars.putDouble("mapHeight", -1);
+    }
+    // returns a nested array of map information.
+    mapInfo = [
+    "Map: " + currentMap,
+    " Spawn: " + spawnY,
+    " Ground: " + groundY,
+    " Prestige: " + prestige.x, prestige.y, prestige.z,
+    " Items: " + items.x, items.y, items.z,
+    " Upgrades: " + upgrades.x, upgrades.y, upgrades.z
+    ]
+    return mapInfo;
+}
+
 
 // Holiday Auto Grinder -->
 function dist_mid() {
